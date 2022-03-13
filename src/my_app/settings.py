6e550692741c91dec8739fa9801bd6d5e4320a0e
Sanjay <sanjarbek2709@gmail.com>
2022-03-13 20:48:52 +0300

    message

--- src/my_app/settings.py
+++ src/my_app/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'my_app.tg',
+    'tg',
 ]
 
 MIDDLEWARE = [
--- src/tg/apps.py
+++ src/tg/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class TgConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'my_app.tg'
+    name = 'tg'
